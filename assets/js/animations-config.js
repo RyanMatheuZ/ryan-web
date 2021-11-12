ce04e43c3fc0375639dcbeb5db00a5c4958da556
Ryan Oliveira <oryan424@gmail.com>
2021-11-12 12:53:03 -0300

    Added dark mode

--- assets/js/animations-config.js
+++ assets/js/animations-config.js
@@ -4,28 +4,28 @@ ScrollReveal().reveal('.scroll--move-up', {
     delay: 150,
     interval: 400,
     duration: 2000
-});
+})
 
 ScrollReveal().reveal('.scroll--move-left', {
     origin: 'left',
     distance: '-100px',
     duration: 2000
-});
+})
 
 ScrollReveal().reveal('.scroll--move-right', {
     origin: 'right',
     distance: '-100px',
     duration: 2000
-});
+})
 
 ScrollReveal().reveal('.scroll--move-scale', {
     scale: 0.5,
     duration: 2000
-});
+})
 
 ScrollReveal().reveal('.scroll--move-rotate', {
     rotate: {
         z: -25
     },
     duration: 2000
-});
+})
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
-// Nav active - MOBILE
-document.querySelector('#btn-menu').addEventListener('click', () => {
-    document.querySelector('#nav-menu').classList.toggle('nav-active');
-});
+const btnMenu = document.querySelector('#btn-menu')
+const navMenu =  document.querySelector('#nav-menu')
+const btnTheme = document.querySelector('#btn-theme')
+const htmlTag = document.querySelector('html')
+
+// Nav active (Mobile)
+btnMenu.addEventListener('click', () => navMenu.classList.toggle('nav-active'))
+
+// Website theme
+btnTheme.addEventListener('click', () => {
+    if (btnTheme.getAttribute('dark-mode') === 'false') {
+        btnTheme.setAttribute('src', './static/icons/website-theme/dark-theme.svg')
+        btnTheme.setAttribute('dark-mode', 'true')
+        htmlTag.classList.add('dark-mode')
+    } else {
+        btnTheme.setAttribute('src', './static/icons/website-theme/light-theme.svg')
+        btnTheme.setAttribute('dark-mode', 'false')
+        htmlTag.classList.remove('dark-mode')
+    }
+})
