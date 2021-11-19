f60d2927bffb14e92f5187d072299fd0209fd8fc
Ryan Oliveira <oryan424@gmail.com>
2021-11-19 13:09:06 -0300

    i18n init

---
+++ locales/languages.js
@@ -0,0 +1,461 @@
+languages = {
+    pt: {
+        header: {
+            about: "Sobre",
+
+            services: "Serviços",
+
+            projects: "Projetos",
+
+            knowledges: "Conhecimentos"
+        },
+
+        sectionInitial: {
+            greeting: "Olá, eu sou o Ryan Oliveira",
+
+            description: "Desenvolvedor Front-End!",
+            
+            btnContact: "Contato",
+
+            btnResume: "Currículo"
+        },
+
+        sectionAbout: {
+            title: "Sobre",
+
+            subtitle: "DEIXE EU ME APRESENTAR",
+
+            paragraphOne: "Meu nome é Ryan Matheus de Oliveira e moro em Matão-SP.",
+
+            paragraphTwo: "Indo para o 3° ano do EM, permanecia em dúvida sobre qual carreira profissional seguir, então decidi fazer um curso técnico na Etec, onde tive o meu primeiro contato com desenvolvimento de sistemas, logo, me fissurei pela área.",
+
+            paragraphThree: "Atualmente estou me graduando em Sistemas para Internet na Fatec Taquaritinga e possuo conhecimentos nos setores de Desenvolvimento Web e Web Design."
+        },
+
+        sectionServices: {
+            title: "Serviços",
+
+            subtitle: "O QUE EU POSSO FAZER POR VOCÊ",
+
+            frontEnd: {
+                title: "Front-End",
+
+                text: "Desenvolvimento da interface gráfica do usuário de um site"
+            },
+
+            responsiveWesites: {
+                title: "Sites Responsivos",
+
+                text: "Sites que se adaptam em diferentes tipos de dispositivos"
+            },
+
+            semanticHtml: {
+                title: "HTML Semântico",
+
+                text: "Melhor compreensão do HTML em motores de busca"
+            },
+
+            webAccessibility: {
+                title: "Acessibilidade Web",
+
+                text: "Sites acessíveis para pessoas com algum tipo de deficiência"
+            },
+
+            webSeo: {
+                title: "SEO Web",
+
+                text: "Técnicas White Hat, para otimização de conteúdo"
+            },
+        },
+
+        sectionProjects: {
+            title: "Projetos",
+
+            subtitle: "MEU CONHECIMENTO NA PRÁTICA",
+
+            technologies: "Tecnologias",
+
+            btnGitHub: "Código",
+
+            btnAccess: "Projeto",
+
+            farmino: {
+                title: "Farmino",
+
+                description: "Projeto pessoal, sobre uma loja virtual com foco na venda de produtos orgânicos. Nele Foram utilizados Vue Router, Vuex, Vue Meta e Vuelidate."
+            },
+
+            voidX: {
+                title: "Void X",
+
+                description: "Projeto desenvolvido para uma prova na faculdade. Com o requisito de ser um site para um produto revolucionário e validação de formulário com Vuelidate."
+            },
+
+            fatecMatão: {
+                title: "Fatec Matão",
+
+                description: "Projeto desenvolvido em grupo, durante as aulas de Projeto Integrador. Fui responsável pela página inicial e internacionalização (i18n)."
+            },
+
+            soffitConsulting: {
+                title: "Soffit Consultoria",
+
+                description: "Projeto pessoal, sobre uma consultoria fictícia, com serviços em planos de negócios, análises, estratégias, vendas, marketing e muito mais."
+            },
+
+            rickAndMorty: {
+                title: "Rick and morty",
+
+                description: "Consumindo a API da série Rick e Morty, com fetch API. Essa API contém várias informações sobre os personagens, como o nome, gênero, origem, etc."
+            },
+
+            petitGlace: {
+                title: "Petit Glace",
+
+                description: "Projeto pessoal, sobre uma sorveteria fictícia. O usuário pode realizar uma filtragem de produtos, seja pela origem ou preço do produto."
+            },
+
+            vruum: {
+                title: "Vruum",
+
+                description: "Projeto pessoal, sobre uma autoescola fictícia. A mesma oferece cursos tradicionais de direção e de aperfeiçoamento no trânsito."
+            },
+
+            cloudMaster: {
+                title: "Cloud Master",
+
+                description: "Projeto pessoal, sobre uma empresa fictícia. Ela possui serviços de desenvolvimento web, hospedagem e planos Wi-Fi."
+            }       
+        },
+
+        sectionKnowledges: {
+            title: "Conhecimentos",
+
+            subtitle: "TUDO QUE ADQUIRI ATÉ O MOMENTO",
+
+            html: {
+                title: "HTML",
+
+                text: "Linguagem de marcação, usada na construção de sites"
+            },
+
+            css: {
+                title: "CSS",
+
+                text: "Folha de estilos em cascata, para estilização de sites"
+            },
+
+            sass: {
+                title: "Sass / SCSS",
+
+                text: "Pré-processador, basicamente CSS com super poderes"
+            },
+
+            less: {
+                title: "Less",
+
+                text: "Pré-processador, que foi influenciado pelo Sass"
+            },
+
+            bootstrap: {
+                title: "Bootstrap",
+
+                text: "Framework CSS, o grande dinossauro do Front-End"
+            },
+
+            bulma: {
+                title: "Bulma",
+
+                text: "Framework CSS, foi inspirado no Bootstrap e não requer JS"
+            },
+
+            tailwindcss: {
+                title: "Tailwind CSS",
+
+                text: "Framework CSS moderninho, baseado em utilitários"
+            },
+
+            javascript: {
+                title: "JavaScript (ES)",
+
+                text: "Linguagem queridinha dos Desenvolvedores Web"
+            },
+
+            jquery: {
+                title: "jQuery",
+
+                text: "Outro dinossauro do Front-End, porém ele é uma biblioteca JS"
+            },
+
+            vue: {
+                title: "Vue.js",
+
+                text: "Framework progressivo, basicamente o melhor do mercado de JS"
+            },
+
+            nuxt: {
+                title: "Nuxt",
+
+                text: "Framework JS, Batman tem o Robin, o Vue tem o Nuxt"
+            },
+
+            git: {
+                title: "Git",
+
+                text: "Versionador de código, para salvar snapshots de seus projetos"
+            }
+        },
+
+        sectionContact: {
+            title: "Algum projeto em mente?",
+
+            text: "Com dúvida ou problema em algum projeto, não hesite em me chamar!",
+
+            btnContact: "Entrar em contato"
+        },
+
+        footer: {
+            form: {
+                title: "Me mande uma mensagem...",
+
+                yourName: "Seu nome:",
+
+                yourEmail: "Seu E-mail:",
+
+                yourMessage: "Sua mensagem:",
+
+                btnSend: "Enviar"
+            }
+        }
+    },
+
+    en: {
+        header: {
+            about: "",
+
+            services: "",
+
+            projects: "",
+
+            knowledges: ""
+        },
+
+        sectionInitial: {
+            greeting: "",
+
+            description: "",
+            
+            btnContact: "",
+
+            btnResume: ""
+        },
+
+        sectionAbout: {
+            title: "",
+
+            subtitle: "",
+
+            paragraphOne: "",
+
+            paragraphTwo: "",
+
+            paragraphThree: ""
+        },
+
+        sectionServices: {
+            title: "",
+
+            subtitle: "",
+
+            frontEnd: {
+                title: "",
+
+                text: ""
+            },
+
+            responsiveWesites: {
+                title: "",
+
+                text: ""
+            },
+
+            semanticHtml: {
+                title: "",
+
+                text: ""
+            },
+
+            webAccessibility: {
+                title: "",
+
+                text: ""
+            },
+
+            webSeo: {
+                title: "",
+
+                text: ""
+            },
+        },
+
+        sectionProjects: {
+            title: "",
+
+            subtitle: "",
+
+            technologies: "",
+
+            btnGitHub: "",
+
+            btnAccess: "",
+
+            farmino: {
+                title: "",
+
+                description: ""
+            },
+
+            voidX: {
+                title: "Void X",
+
+                description: ""
+            },
+
+            fatecMatão: {
+                title: "Fatec Matão",
+
+                description: ""
+            },
+
+            soffitConsulting: {
+                title: "Soffit Consultoria",
+
+                description: ""
+            },
+
+            rickAndMorty: {
+                title: "Rick and morty",
+
+                description: ""
+            },
+
+            petitGlace: {
+                title: "Petit Glace",
+
+                description: ""
+            },
+
+            vruum: {
+                title: "Vruum",
+
+                description: ""
+            },
+
+            cloudMaster: {
+                title: "Cloud Master",
+
+                description: ""
+            }       
+        },
+
+        sectionKnowledges: {
+            title: "",
+
+            subtitle: "",
+
+            html: {
+                title: "HTML",
+
+                text: ""
+            },
+
+            css: {
+                title: "CSS",
+
+                text: ""
+            },
+
+            sass: {
+                title: "Sass / SCSS",
+
+                text: ""
+            },
+
+            less: {
+                title: "Less",
+
+                text: ""
+            },
+
+            bootstrap: {
+                title: "Bootstrap",
+
+                text: ""
+            },
+
+            bulma: {
+                title: "Bulma",
+
+                text: ""
+            },
+
+            tailwindcss: {
+                title: "Tailwind CSS",
+
+                text: ""
+            },
+
+            javascript: {
+                title: "JavaScript (ES)",
+
+                text: ""
+            },
+
+            jquery: {
+                title: "jQuery",
+
+                text: ""
+            },
+
+            vue: {
+                title: "Vue.js",
+
+                text: ""
+            },
+
+            nuxt: {
+                title: "Nuxt",
+
+                text: ""
+            },
+
+            git: {
+                title: "Git",
+
+                text: ""
+            }
+        },
+
+        sectionContact: {
+            title: "",
+
+            text: "",
+
+            btnContact: ""
+        },
+
+        footer: {
+            form: {
+                title: "",
+
+                yourName: "",
+
+                yourEmail: "",
+
+                yourMessage: "",
+
+                btnSend: ""
+            }
+        }
+    }
+}
