f986317f5bca5f7b84c962a244ab63e58b7f8c16
Ryan Oliveira <oryan424@gmail.com>
2021-11-18 21:58:32 -0300

    Add effects and Up button

--- assets/js/animations-config.js
+++ assets/js/animations-config.js
@@ -1,31 +1,31 @@
-ScrollReveal().reveal('.scroll--move-up', {
-    origin: 'top',
-    distance: '-50px',
+ScrollReveal().reveal(".scroll--move-up", {
+    origin: "top",
+    distance: "-50px",
     delay: 150,
     interval: 400,
-    duration: 2000
-})
+    duration: 2300
+});
 
-ScrollReveal().reveal('.scroll--move-left', {
-    origin: 'left',
-    distance: '-100px',
-    duration: 2000
-})
+ScrollReveal().reveal(".scroll--move-left", {
+    origin: "left",
+    distance: "100px",
+    duration: 2500
+});
 
-ScrollReveal().reveal('.scroll--move-right', {
-    origin: 'right',
-    distance: '-100px',
-    duration: 2000
-})
+ScrollReveal().reveal(".scroll--move-right", {
+    origin: "right",
+    distance: "100px",
+    duration: 2500
+});
 
-ScrollReveal().reveal('.scroll--move-scale', {
+ScrollReveal().reveal(".scroll--move-scale", {
     scale: 0.5,
     duration: 2000
-})
+});
 
-ScrollReveal().reveal('.scroll--move-rotate', {
+ScrollReveal().reveal(".scroll--move-rotate", {
     rotate: {
         z: -25
     },
     duration: 2000
-})
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,24 @@
-const btnMenu = document.querySelector('#btn-menu')
-const navMenu =  document.querySelector('#nav-menu')
-const btnTheme = document.querySelector('#btn-theme')
-const htmlTag = document.querySelector('html')
+const btnMenu = document.querySelector("#btn-menu");
+const navMenu =  document.querySelector("#nav-menu");
+const btnUp = document.querySelector("#btn-up");
+const btnTheme = document.querySelector("#btn-theme");
+const htmlTag = document.querySelector("html");
 
 // Nav active (Mobile)
-btnMenu.addEventListener('click', () => navMenu.classList.toggle('nav-active'))
+btnMenu.addEventListener("click", () => navMenu.classList.toggle("nav-active"))
+
+// Button up
+btnUp.addEventListener("click", () => scrollTo(0, 0));
 
 // Website theme
-btnTheme.addEventListener('click', () => {
-    if (btnTheme.getAttribute('dark-mode') === 'false') {
-        btnTheme.setAttribute('src', './static/icons/website-theme/dark-theme.svg')
-        btnTheme.setAttribute('dark-mode', 'true')
-        htmlTag.classList.add('dark-mode')
+btnTheme.addEventListener("click", () => {
+    if (btnTheme.getAttribute("dark-mode") === "false") {
+        btnTheme.setAttribute("src", "./static/icons/website-theme/dark-theme.svg");
+        btnTheme.setAttribute("dark-mode", "true");
+        htmlTag.classList.add("dark-mode");
     } else {
-        btnTheme.setAttribute('src', './static/icons/website-theme/light-theme.svg')
-        btnTheme.setAttribute('dark-mode', 'false')
-        htmlTag.classList.remove('dark-mode')
+        btnTheme.setAttribute("src", "./static/icons/website-theme/light-theme.svg");
+        btnTheme.setAttribute("dark-mode", "false");
+        htmlTag.classList.remove("dark-mode");
     }
-})
+});
