df3e6ca292469199d81a3eb5e66a218f5297d22a
RyanMatheuZ <contato.ryanoliveira@outlook.com>
2022-11-23 10:33:31 -0300

    feat: add astrobit project

--- assets/js/form.js
+++ assets/js/form.js
@@ -9,21 +9,21 @@ const inputEmailError = document.getElementById("input-email-error");
 const inputMessageError = document.getElementById("input-message-error");
 
 btnSend.addEventListener("click", () => {
-	inputNameError.style.display = "none";
-	inputEmailError.style.display = "none";
-	inputMessageError.style.display = "none";
+  inputNameError.style.display = "none";
+  inputEmailError.style.display = "none";
+  inputMessageError.style.display = "none";
 
-    if (!inputName.value) {
-		inputNameError.style.display = "block";
-        nameResult.focus();
-		navigator.vibrate(200);
-    } else if (!inputEmail.value) {
-		inputEmailError.style.display = "block";
-		emailResult.focus();
-		navigator.vibrate(200);
-    } else if (!inputMessage.value) {
-		inputMessageError.style.display = "block";
-        messageResult.focus();
-		navigator.vibrate(200);
-    }
+  if (!inputName.value) {
+    inputNameError.style.display = "block";
+    nameResult.focus();
+    navigator.vibrate(200);
+  } else if (!inputEmail.value) {
+    inputEmailError.style.display = "block";
+    emailResult.focus();
+    navigator.vibrate(200);
+  } else if (!inputMessage.value) {
+    inputMessageError.style.display = "block";
+    messageResult.focus();
+    navigator.vibrate(200);
+  }
 });
